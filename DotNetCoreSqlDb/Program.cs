e4bc438f1310fd8d20a390f0d63a85326af93358
danihagosbl <168411661+danihagosbl@users.noreply.github.com>
2024-05-14 09:01:05 +0000

    updated start up config

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -4,6 +4,14 @@ using DotNetCoreSqlDb.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add database context and cache
+if(builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<MyDatabaseContext>(options =>
+      options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
+} else {
+   builder.Services.AddDbContext<MyDatabaseContext>(options =>
+      options.UseSqlServer(Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING")));
+}
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
 builder.Services.AddDistributedMemoryCache();
